c661061b46b92b2eea753386d4e4e60113ecbfcb
RohitSubhash <89678238+RohitSubhash@users.noreply.github.com>
2021-08-29 18:04:47 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -1,19 +1,12 @@
-num1 = int(input('Enter first number: '))
-num2 = int(input('Enter second number: '))
-print( num1 ,'+', num2 , '=' , num1+num2 )
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
+print( a ,'+', b , '=' , a+b )
 
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
 c = a + b
 print("%d + %d = %d\n"%(a, b, c))
 
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
+
 sum = a + b
 print("The sum of {0} and {1} is {2}".format(a, b, sum))
 
 print('The sum is %d' %(int(input('Enter first number: ')) + int(input('Enter second number: '))))
-
-
-
-
